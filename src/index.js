312c1b77981884af1d60be15aa24c3e346f1ae9f
samroodAli <devsamrood@gmail.com>
2021-07-23 21:00:57 +0530

    Config:Add env file to .gitignore

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log("Hello world");
+function request(location, units = "metric") {
+  return `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=&units=${units}`;
+}
+
+async function getWeather(location) {
+  const data = await fetch(request(location));
+  return data.json();
+}
+
+const data = getWeather("Alexandria");
+console.log(data);
