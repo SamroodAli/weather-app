8ccf9ec176977bf530df274e9007b3ec6ea62c5a
samroodAli <devsamrood@gmail.com>
2021-07-24 17:54:15 +0530

    Asset:Import bootstrap to the application and remove styles in scss file

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const webpack = require("webpack");
 
 module.exports = {
   entry: "./src/index.js",
@@ -7,6 +8,13 @@ module.exports = {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
   },
+  plugins: [
+    new webpack.DefinePlugin({
+      "process.env": {
+        API_KEY: JSON.stringify(process.env.API_KEY),
+      },
+    }),
+  ],
   module: {
     rules: [
       {
