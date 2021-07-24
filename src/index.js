d29fc55fc38553ca16f728a47aab6cd7c827169d
samroodAli <devsamrood@gmail.com>
2021-07-24 18:21:25 +0530

    Asset:Import dotenv webpack for securing open weather map keys

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
 import "./style.scss";
 
-function request(location, units = "metric") {
+function fromOpenWeatherMap(location, units = "metric") {
   return `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=&units=${units}`;
 }
 
 async function getWeather(location) {
-  const data = await fetch(request(location));
+  const data = await fetch(fromOpenWeatherMap(location));
   return data.json();
 }
 
-const data = getWeather("Alexandria");
-console.log(data);
-
+// const data = getWeather("Alexandria");
 console.log("Hello programmer");
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const path = require("path");
-const webpack = require("webpack");
+const Dotenv = require("dotenv-webpack");
 
 module.exports = {
   entry: "./src/index.js",
@@ -12,13 +12,7 @@ module.exports = {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
   },
-  plugins: [
-    new webpack.DefinePlugin({
-      "process.env": {
-        API_KEY: JSON.stringify(process.env.API_KEY),
-      },
-    }),
-  ],
+  plugins: [new Dotenv()],
   module: {
     rules: [
       {
