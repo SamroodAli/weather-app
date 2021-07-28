59318f709984160315e3c23445bfc417a957bbe3
samroodAli <devsamrood@gmail.com>
2021-07-28 14:07:03 +0530

    Test:Test env from github action console

--- src/index.js
+++ src/index.js
@@ -1,22 +1,24 @@
-import './style.scss';
+import "./style.scss";
 
 const { API_KEY } = process.env;
+console.log(API_KEYs);
 
 const eleId = (id) => document.getElementById(id);
 
-const fromOpenWeatherMap = (location, units = 'metric') => `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${API_KEY}&units=${units}`;
+const fromOpenWeatherMap = (location, units = "metric") =>
+  `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${API_KEY}&units=${units}`;
 
-const loader = eleId('loader');
+const loader = eleId("loader");
 
 const getWeather = async (location) => {
   const placeholder = { main: { temp: 0 } };
 
-  loader.classList.toggle('none');
+  loader.classList.toggle("none");
 
   try {
     const response = await fetch(fromOpenWeatherMap(location));
     const data = await response.json();
-    loader.classList.toggle('none');
+    loader.classList.toggle("none");
 
     if (response.status === 200) {
       return data;
@@ -27,16 +29,16 @@ const getWeather = async (location) => {
   }
 };
 
-const form = eleId('form');
-const location = eleId('location');
+const form = eleId("form");
+const location = eleId("location");
 
-const city = eleId('city');
-const tempatureElement = eleId('temperature');
+const city = eleId("city");
+const tempatureElement = eleId("temperature");
 let temperature = 0;
 
 const updateCelius = () => {
   if (temperature === 0) {
-    tempatureElement.innerHTML = 'No weather data available';
+    tempatureElement.innerHTML = "No weather data available";
   } else {
     tempatureElement.innerHTML = `Temperature: ${temperature} °C`;
   }
@@ -46,26 +48,26 @@ const calcFahrenheit = () => (temperature * (9 / 5) + 32).toFixed(2);
 
 const updateFahrenheit = () => {
   if (temperature === 0) {
-    tempatureElement.innerHTML = 'No weather data available';
+    tempatureElement.innerHTML = "No weather data available";
   } else {
     tempatureElement.innerHTML = `Temperature: ${calcFahrenheit()} °F`;
   }
 };
 
-const weatherCard = eleId('weatherCard');
+const weatherCard = eleId("weatherCard");
 
 const updateWeatherData = (data) => {
   temperature = Number(data.temp);
-  city.innerHTML = 'Weather Data';
+  city.innerHTML = "Weather Data";
   if (temperature) {
     city.innerHTML += ` for ${location.value}`;
   }
   if (temperature < 15) {
-    document.body.style.backgroundColor = '#0f5ebb';
+    document.body.style.backgroundColor = "#0f5ebb";
   } else {
-    document.body.style.backgroundColor = '#ff0000';
+    document.body.style.backgroundColor = "#ff0000";
   }
-  weatherCard.classList.remove('hidden');
+  weatherCard.classList.remove("hidden");
   updateCelius();
 };
 
@@ -79,9 +81,9 @@ const onSubmit = (e) => {
   getWeather(location.value).then(displayWeatherData);
 };
 
-form.addEventListener('submit', onSubmit);
+form.addEventListener("submit", onSubmit);
 
-const fahrenheit = eleId('fahrenheit');
-fahrenheit.addEventListener('click', updateFahrenheit);
-const celsius = eleId('celsius');
-celsius.addEventListener('click', updateCelius);
+const fahrenheit = eleId("fahrenheit");
+fahrenheit.addEventListener("click", updateFahrenheit);
+const celsius = eleId("celsius");
+celsius.addEventListener("click", updateCelius);
